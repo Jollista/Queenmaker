d2034e9b1acfbe2f0d211f26034bfc12b5d4f6e5
Sulfurous-Impersonation <june@watson-net.com>
2023-04-21 23:26:44 -0700

    Added drone wall bonking in patrol

--- Drone/Drone.gd
+++ Drone/Drone.gd
@@ -1,6 +1,11 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 var bodies_list = []
+
+# movement
+@export var speed = 50
+var direction = -1
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # damage dealt with attack
 @export var damage = 1
@@ -9,6 +14,7 @@ var bodies_list = []
 @onready var hitbox = $CollisionShape2D
 # attack range area2d
 @onready var attack_range = $AttackRange
+@onready var attack_collider = $AttackRange/CollisionShape2D
 
 # sprite
 @onready var sprite = $Sprite2D
@@ -23,15 +29,22 @@ var attacking = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	patrol()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# handle falling
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	# determine action
 	if not attacking: # if not attacking
 		if bodies_list.is_empty(): # and bodies_list is empty
-			patrol() # just patrol
+			patrol(delta) # just patrol
 		elif timer.is_stopped(): # else, if bodies_list is not empty and timer is stopped, contains Player
 			attack(bodies_list[0]) # attack 'em!
+	
+	move_and_slide() # update movement
 
 # on body entered attack range
 func _on_attack_range_body_entered(body):
@@ -45,8 +58,23 @@ func _on_attack_range_body_exited(body):
 		bodies_list.erase(body)
 
 # basic patrol, just walk around until something wanders into attack range
-func patrol():
+func patrol(delta):
+	if is_on_wall() or is_at_edge():
+		turn()
+	
+	velocity.x = direction * speed # move
+	move_and_slide() # update movement
+
+# returns true if at the edge of a platform, else false
+func is_at_edge():
 	pass
+
+# turns the drone around
+func turn():
+	sprite.flip_h = true # flip sprite
+	attack_collider.position.x = -attack_collider.position.x # flip attack range
+	direction = -direction # change direction
+
 
 # attack and deal damage to things within range
 func attack(body):
